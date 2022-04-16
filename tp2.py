cd20aade693fd19780c53f71d7f7b74f475bbbe6
Andre Figo <nefigo2001@gmail.com>
2022-04-16 02:14:45 +0100

    songs' features extracted (all 900)

--- tp2.py
+++ tp2.py
@@ -1,10 +1,12 @@
 import numpy as np
 import os
+import warnings
 import librosa
 import scipy
 
 SR = 22050
 MONO = True
+
 
 def import_csv(filename:str) -> np.ndarray:
     ans = np.genfromtxt(filename, skip_header = 1, delimiter=',')
@@ -32,8 +34,12 @@ def calculate_stats(array):
     return np.array([mean,std,skewness, kurtosis,median, maximum, minimum])
 
 def features_librosa(dirname:str) -> np.ndarray:    
-    ans=[]
+    ans=np.array([])
+    i=0
     for filename in os.listdir(dirname):
+        print(filename)
+        i+=1
+        print(i)
         # spectral features
         y,fs = librosa.load(dirname+'/'+filename, sr = SR, mono = MONO)
         mfccs = librosa.feature.mfcc(y, sr=SR, n_mfcc=13)
@@ -49,29 +55,45 @@ def features_librosa(dirname:str) -> np.ndarray:
         rms = librosa.feature.rms(y)
         zcr = librosa.feature.zero_crossing_rate(y)
         f0 = librosa.yin(y, sr=SR, fmin=20, fmax=11025)
+        print( "f0 shape ",f0.shape)
         f0[f0==11025]=0
-        all_features_array = np.vstack((mfccs, spcentroid, spband,spcontrast, spflatness,sprolloff, rms, zcr,f0))
+        all_features_array = np.vstack((mfccs, spcentroid, spband, spcontrast, spflatness, sprolloff, f0, rms, zcr))
+        print( "all shape ",all_features_array.shape)
         all_stats = np.apply_along_axis(calculate_stats, 1, all_features_array).flatten()
+        print( "all shape 2 ",all_stats.shape)
+
 
         tempo = librosa.beat.tempo(y,sr=SR)
         aid = np.append(all_stats, tempo)
-
-        ans.append(aid)
-        # break
+        print(aid)
+        print("shape aid ", aid.shape)
+        if i==1:
+            ans = np.array(aid)
+        else:
+            ans= np.vstack((ans,aid))
     print("ans: ",ans)
+    print("ans shape: ",ans.shape)
     ans = np.array(ans)
     return normalize_features(ans)
 
-
-def main() -> None:
+def save_normalized_features():
     data = import_csv("dataset/top100_features.csv")
     n_data = normalize_features(data)
     print(n_data)
-    print('shape: ',n_data.shape)
+    print('shape: ', n_data.shape)
     export_csv('dataset/normalized_features.csv', n_data)
+    return n_data
+
+
+def main() -> None:
+    warnings.filterwarnings("ignore")
+    # save_normalized_features()
     features_norm_obtained = features_librosa('dataset/allSongs')
-    export_csv('dataset/normalized_features2_2.csv', n_data)
+
+    export_csv('dataset/song_features.csv', features_norm_obtained)
+
 
 if __name__ == "__main__":
+
     main()
 
